72be040ba6d882de76a2c675a8cace777b5457d2
kpaveliev <kpaveliev@gmail.com>
2022-03-28 11:39:16 +0300

    Bookmarks buttons formatting
    - bookmarks opacity =1 when post is bookmarked
    - changed views and templates to allow that

--- app.py
+++ app.py
@@ -29,19 +29,24 @@ def post_by_id(post_id):
     comments = get_comments_by_post_id(COMMENTS, post_id)
     return render_template('post.html', post=tagged_post, comments=comments)
 
+
 # Views by user, tag, search by word
 @app.route("/users/<user_name>")
 def posts_by_user(user_name):
     """Page with all the posts of the user"""
     posts = get_posts_by_user(POSTS, user_name)
-    return render_template('user-feed.html', posts=posts, user_name=user_name)
+    bookmarks = get_data(BOOKMARKS)
+    return render_template('user-feed.html', posts=posts, bookmarks=bookmarks,
+                           user_name=user_name)
 
 
 @app.route("/tag/<tag_name>")
 def posts_by_tag(tag_name):
     """Page with all the posts containing the tag"""
     posts_found = get_posts_for_word(POSTS, f'#{tag_name}')
-    return render_template('tag.html', posts=posts_found, tag_name=tag_name.capitalize())
+    bookmarks = get_data(BOOKMARKS)
+    return render_template('tag.html', posts=posts_found,
+                           bookmarks=bookmarks, tag_name=tag_name.capitalize())
 
 
 @app.route("/search")
@@ -49,7 +54,8 @@ def posts_search():
     """Page with all the posts containing some word"""
     searched_word = request.args.get('s')
     posts_found = get_posts_for_word(POSTS, searched_word)
-    return render_template('search.html', posts=posts_found)
+    bookmarks = get_data(BOOKMARKS)
+    return render_template('search.html', posts=posts_found, bookmarks=bookmarks)
 
 
 # Views for bookmarks
@@ -88,11 +94,6 @@ def bookmarks_remove(post_id):
     write_data(BOOKMARKS, bookmarks)
     return redirect("/", code = 302)
 
-
-# Add a route to display images from uploads folder
-# @app.route("/resources/uploads/<path:path>")
-# def static_dir(path):
-#     return send_from_directory("/resources/uploads/", path)
 
 if __name__ == '__main__':
     app.run()

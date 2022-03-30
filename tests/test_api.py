b56ccb9dcbbbe63c755e1dc674eb13438d73a05a
kpaveliev <kpaveliev@gmail.com>
2022-03-30 16:12:23 +0300

    API test added
    - in test_api 2 API tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,23 @@
-# import pytest
-# import os, sys
-#
-# path = os.path.abspath('.')
-# sys.path.insert(1, path)
-#
-# from app import POSTS, COMMENTS
-# from api.api import api_posts
-#
-# # Define tests for functions
-# def test_api_posts():
-#     """Check if json loads correctly"""
-#     result = api_posts()
-#     assert isinstance(result, list) == 8, "Ошибка загрузки файла"
+import pytest
+import os, sys
+path = os.path.abspath('.')
+sys.path.insert(1, path)
+from app import app
+
+# Define tests for view functions
+def test_api_posts():
+    """Check if json with all the posts loads correctly"""
+    response = app.test_client().get('/api/posts/')
+    posts = response.json
+    keys_to_check = ('poster_name', 'poster_avatar', 'pic', 'content', 'views_count', 'likes_count', 'pk')
+    assert isinstance(posts, list), "Ошибка API (загрузка постов): выгружается не список"
+    assert sorted(tuple(posts[0].keys())) == sorted(keys_to_check), "Ошибка API (загрузка постов): нет нужных ключей"
+
+
+def test_api_post_by_id():
+    """Check if json with specified post loads correctly"""
+    response = app.test_client().get('/api/posts/1')
+    post = response.json
+    keys_to_check = ('poster_name', 'poster_avatar', 'pic', 'content', 'views_count', 'likes_count', 'pk')
+    assert isinstance(post, dict), "Ошибка API (загрузка поста по ID): выгружается не словарь"
+    assert sorted(tuple(post.keys())) == sorted(keys_to_check), "Ошибка API (загрузка поста по ID): нет нужных ключей"
